86e9566fcdecef41c3297a6686604567dbff6b57
Ahmad Ambari <ahmadambari044@gmail.com>
2026-04-29 23:56:24 +0700

    fix: outDir to dist

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Portofolio-Ari-V5", // <--- TAMBAHKAN BARIS INI
   build: {
-    outDir: "docs", // <--- Penting untuk GitHub Pages (membutuhkan folder 'docs')
+    outDir: "dist",
     emptyOutDir: true,
-  },
-  server: {
-    // Tambahkan ini jika muncul error CORS atau 404 pada aset
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
-    },
   },
 });
