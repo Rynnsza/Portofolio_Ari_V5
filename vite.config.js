b4f9a554322a64d7cf0bfba9993bb5a5c4023457
Ahmad Ambari <ahmadambari044@gmail.com>
2026-04-29 23:38:35 +0700

    fix: update vite config for vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Portofolio-Ari-V5", // <--- TAMBAHKAN BARIS INI
+  build: {
+    outDir: "docs", // <--- Penting untuk GitHub Pages (membutuhkan folder 'docs')
+    emptyOutDir: true,
+  },
+  server: {
+    // Tambahkan ini jika muncul error CORS atau 404 pada aset
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    },
+  },
+});
